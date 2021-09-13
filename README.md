ed51dbbf10504da8a9a5872a674e01deeeb7b2eb
YosafatM <yosafat_martinez21@hotmail.com>
2021-09-13 18:45:00 -0500

    Actualizacion a Flutter 2

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # UFlutter-mi-card
 Primera aplicación en Flutter
+Idea del curso de la Dr. Yu: https://www.udemy.com/course/flutter-bootcamp-with-dart/
 
-Esta aplicación forma parte del curso de Flutter: https://www.udemy.com/course/flutter-bootcamp-with-dart/
+Actualizado para que funcione en Flutter 2.
+![Imagen de muestra](./images/Mi-Card.jpg "Mi Card")
--- lib/main.dart
+++ lib/main.dart
@@ -13,72 +13,129 @@ class MainApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.grey,
-                backgroundImage: AssetImage('images/avatar.jpg'),
+              SizedBox(
+                height: 30,
               ),
-              Text(
-                'Yosafat Martinez',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  color: Colors.white,
-                  fontSize: 40,
-                ),
+              Header(
+                  name: "Yosafat Coronel",
+                  job: "Ingeniero en Sistemas",
+                  imagePath: "images/Perfil.jpg"
               ),
-              Text(
-                'Estudiante de Flutter',
-                style: TextStyle(
-                  fontFamily: 'Sans',
-                  fontSize: 20,
-                ),
+              SizedBox(
+                height: 100,
               ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.call,
-                      size: 25.0,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      '+52-5525-9083',
-                      style: TextStyle(
-                        color: Colors.teal,
-                        fontSize: 20.0,
-                        fontFamily: 'Sans',
-                      ),
-                    ),
+              Column(
+                children: [
+                  DataCard(
+                    icon: Icons.call,
+                    text: '+52 55 2800 9775',
                   ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.mail,
-                      size: 20.0,
-                    ),
-                    title: Text(
-                      'yosafatmartinez@qwip-app.com',
-                      style: TextStyle(
-                        fontFamily: 'Sans',
-                        color: Colors.teal,
-                        fontSize: 18.0,
-                      ),
-                    ),
+                  DataCard(
+                    icon: Icons.mail_rounded,
+                    text: 'yosafatmartinez21@gmail.com',
                   ),
-                ),
-              ),
+                  DataCard(
+                    icon: Icons.access_time,
+                    text: '9 AM a 7 PM',
+                  ),
+                  DataCard(
+                    icon: Icons.add_location,
+                    text: 'México, México',
+                  ),
+                  DataCard(
+                    icon: Icons.alternate_email,
+                    text: 'Github: Yosafat Martinez',
+                  ),
+                  DataCard(
+                    icon: Icons.apartment,
+                    text: 'Doveeto & DSC ESCOM',
+                  ),
+                ],
+              )
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  final String name, job, imagePath;
+
+  const Header({
+    @required this.name,
+    @required this.job,
+    @required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        CircleAvatar(
+          radius: 40.0,
+          backgroundColor: Colors.grey,
+          backgroundImage: AssetImage(this.imagePath),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              this.name,
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 40,
+              ),
+            ),
+            Text(
+              this.job,
+              style: TextStyle(
+                fontFamily: 'Sans',
+                fontSize: 20,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+
+class DataCard extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const DataCard({
+    @required this.icon,
+    @required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: ListTile(
+          leading: Icon(
+            this.icon,
+            color: Colors.teal,
+            size: 20.0,
+          ),
+          title: Text(
+            this.text,
+            style: TextStyle(
+              fontFamily: 'Sans',
+              color: Colors.black,
+              fontSize: 18.0,
+            ),
           ),
         ),
       ),
